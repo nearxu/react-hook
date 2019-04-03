880b28cf7583b832b5db6f5931df676a5df24af2
nearxu <2448895924@qq.com>
2019-04-04 00:15:16 +0800

    lose context

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './test-ui-hook';
+import {Alerts} from './alert';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Alerts />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
